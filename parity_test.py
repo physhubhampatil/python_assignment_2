576c5cc1845d3942515dc4913bc5fd17c298cb40
ShubhamPatil <102431396+physhubhampatil@users.noreply.github.com>
2026-02-24 18:41:37 +0530

    Add files via upload

---
+++ parity_test.py
@@ -0,0 +1,8 @@
+#Take number as input from an user.
+N = int(input("Enter a number: "))
+
+#Check whether the number is odd or even and print the result.
+if N % 2 == 0:
+    print(f"{N} is an even number.")
+else:
+    print(f"{N} is an odd number.")
